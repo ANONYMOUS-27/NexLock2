3fde4bcd693d631cf8e69e4063f8a3d258b3001a
ANONYMOUS-27 <nexlock@users.noreply.github.com>
2026-09-04 00:25:33 -0300

    Corrigir biometria e Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,7 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.activity:activity-compose:1.11.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation("androidx.compose.material3:material3:1.4.0")
     implementation("androidx.compose.ui:ui:1.9.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.9.1")
